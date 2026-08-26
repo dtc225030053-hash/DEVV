3904dc0df8d2dc980e9d91af5a79b3d7f9065a92
dtc225030053-hash <dtc225030053@ictu.edu.vn>
2026-08-26 16:26:13 +0700

    solve conflict

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
-#define LED_PIN     2           // đổi chân LED
+#define LED_PIN     13          // giữ nguyên chân LED
 #define SENSOR_PIN A0
-int threshold = 600;           // tăng ngưỡng
+int threshold = 300;           // giảm ngưỡng
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
@@ -11,7 +11,5 @@ void setup() {
 void loop() {
     int value = analogRead(SENSOR_PIN);
     if (value > threshold)
-        digitalWrite(LED_PIN, HIGH);
-    else
-        digitalWrite(LED_PIN, LOW);
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
 }
